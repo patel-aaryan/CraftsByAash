18901f8b9d0dc9bf1e6b7224a8088563ecb8fed8
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2025-03-01 21:32:50 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["localhost", "https://crafts-by-aash.vercel.app/"],
+    unoptimized: false,
+  },
   reactStrictMode: true,
 };
 
